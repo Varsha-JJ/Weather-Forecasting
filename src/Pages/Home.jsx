67534bf9be35e244323b3fe26fb3fa59e461efa6
Varsha-J-J <151743853+Varsha-J-J@users.noreply.github.com>
2023-12-01 18:28:46 +0530

    main home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
+const Home = () => {
+  return (
+    <div className='d-flex justify-content-center mt-5'>
+        <Card style={{width:'1200px', height: '650px'}}>
+            <Card.Body>
+                
+            </Card.Body>
+        </Card>
+    </div>
+  )
+}
+
+export default Home
